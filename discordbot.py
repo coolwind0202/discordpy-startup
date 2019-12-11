c0ee12e622c04b1149f5cab8e4e2bf9b6d5f7951
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-12-11 19:07:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,49 @@
+import discord
 from discord.ext import commands
-import os
-import traceback
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.command(aliases=["connect","summon"])
+async def join(ctx):
+    """Botをボイスチャンネルに入室させます。"""
+    channel = ctx.author.voice.channel
+    if not channel:
+        await ctx.send("先にボイスチャンネルに入っている必要があります。")
+        return
+
+    await channel.connect()
+    print("connected to:",channel.name)
 
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+@bot.command(aliases=["disconnect","bye"])
+async def leave(ctx):
+    """Botをボイスチャンネルから切断します。"""
+    voice_client = ctx.message.guild.voice_client
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, 'original', error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    await voice_client.disconnect()
+    await ctx.send("ボイスチャンネルから切断しました。")
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
 
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
 
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されていません。")
+        return
+
+    file = ctx.message.attachments[0]
+    buffer = await file.read()
+    ffmpeg_audio_source = discord.FFmpegPCMAudio(buffer)
+    
+    voice_client.play(ffmpeg_audio_source)
 bot.run(token)
