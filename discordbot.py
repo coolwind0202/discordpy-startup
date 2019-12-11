edc8e865ab9b5331d73d89f6cba4ea9d20ad0a49
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2019-12-11 18:31:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,13 +1,11 @@
-#from discord.ext import commands
+from discord.ext import commands
 import os
 import traceback
 
 
-#bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-print(token)
 
-"""
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, 'original', error)
@@ -21,4 +19,3 @@ async def ping(ctx):
 
 
 bot.run(token)
-"""
