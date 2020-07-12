9764c49390a5b65f7293fc52472861f40c463459
涼宮 <51913600+coolwind0202@users.noreply.github.com>
2020-07-12 13:27:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,26 +3,13 @@ import discord
 import sys
 import os
 import signal
+import time
+import atexit
 
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
+signal.signal(signal.SIGTERM,lambda signum, frame:print("signal"))
 
-client = discord.Client()
+@atexit.register
+def func():
+    print("atexit")
 
-loop = asyncio.get_event_loop()
-
-def handler():
-    print("SIGTERM received 1")
-    loop.stop()
-
-def handler_2():
-    print("SIGTERM received 2")
-    
-def handler_3(signum,frame):
-    print("SIGTERM received 3")
-
-    
-loop.add_signal_handler(signal.SIGTERM,handler)
-#loop.add_signal_handler(signal.SIGTERM,handler_2)
-signal.signal(signal.SIGTERM,handler_3)
-loop.run_until_complete(client.start(TOKEN))
-print("おい")
+time.sleep(1000)
