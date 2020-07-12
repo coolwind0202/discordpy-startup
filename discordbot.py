932ae7e5dd174d836eea8a8d036ae0927ae365a5
涼宮 <51913600+coolwind0202@users.noreply.github.com>
2020-07-12 12:47:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,33 +1,16 @@
-import discord
-from discord.ext import commands
-
-import os
-import io
 import asyncio
-import time
+import discord
+import sys
 
-import signal
-import atexit
-
-bot = commands.Bot(command_prefix="$")
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_ready():
-    print("ready...")
-
-    
-print("Hello world")
-
-def handler(signum, frame):
-    print(f"received SIGTERM")
-signal.signal(signal.SIGTERM,handler)    
-
-@atexit.register
-def goodbye():
-    print("exit")
+client = discord.Client()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(bot.start(token))
-print("end.")
-#time.sleep(1000)
+
+def handler(signum,frame):
+    print("SIGTERM received")
+    client.close()
+    
+loop.add_signal_handler(signal.SIGTERM,handler)
+loop.run_until_complete(client.start(TOKEN))
