0c70ddd61393acb97cf51b39d2c22516718e31d7
涼宮 <51913600+coolwind0202@users.noreply.github.com>
2020-07-12 13:05:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ loop = asyncio.get_event_loop()
 
 def handler():
     print("SIGTERM received")
-    loop.run_until_complete(client.close())
+    loop.close()
     
 loop.add_signal_handler(signal.SIGTERM,handler)
 loop.run_until_complete(client.start(TOKEN))
