f444c2c92f0d3bc1e3406a4c3e1fd29fe92397bd
涼宮 <51913600+coolwind0202@users.noreply.github.com>
2020-07-12 20:55:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,10 +19,13 @@ def handler(signum,frame):
 signal.signal(signal.SIGTERM,handler)
 
 def run():
+    loop.add_signal_handler(signal.SIGTERM, lambda: loop.stop())
+    print("added handler")
     future = asyncio.ensure_future(client.start(TOKEN), loop=loop)
     future.add_done_callback(lambda:loop.stop())
     #loop.run_until_complete(client.start(TOKEN))
     loop.run_forever()
+    
    
 print("will run")
 run()
