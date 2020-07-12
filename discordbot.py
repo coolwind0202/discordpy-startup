b88e3b173e3c584d567a3b16819a5d861b6e3e04
涼宮 <51913600+coolwind0202@users.noreply.github.com>
2020-07-12 13:12:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,6 @@
 import asyncio
 import discord
 import sys
-import os
 import signal
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -11,8 +10,17 @@ client = discord.Client()
 loop = asyncio.get_event_loop()
 
 def handler():
-    print("SIGTERM received")
-    loop.stop()
+    print("SIGTERM received 1")
+    #loop.stop()
+
+def handler_2():
+    print("SIGTERM received 2")
+    
+def handler_3(signum,frame):
+    print("SIGTERM received 3")
+
     
 loop.add_signal_handler(signal.SIGTERM,handler)
+loop.add_signal_handler(signal.SIGTERM,handler_2)
+signal.signal(signal.SIGTERM,handler_3)
 loop.run_until_complete(client.start(TOKEN))
